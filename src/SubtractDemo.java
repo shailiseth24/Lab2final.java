e80afdae726221539e71a526270e08ff3550e755
shailiseth24 <shailiseth24@gmail.com>
2025-12-19 13:41:24 +0530

    committed

---
+++ src/SubtractDemo.java
@@ -0,0 +1,23 @@
+class Subtract
+{
+    void subtract(int a, int b) {
+        System.out.println("Subtraction of two integers = " + (a - b));
+    }
+    void subtract(int a, int b, int c) {
+        System.out.println("Subtraction of three integers = " + (a - b - c));
+    }
+    void subtract(double a, double b) {
+        System.out.println("Subtraction of two doubles = " + (a - b));
+    }
+}
+
+public class SubtractDemo
+{
+    public static void main(String[] args)
+    {
+        Subtract s = new Subtract();
+        s.subtract(10, 5);
+        s.subtract(20, 5, 3);
+        s.subtract(15.5, 4.2);
+    }
+}
